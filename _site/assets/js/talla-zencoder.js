60c034c916169de9cc6918c73b6093d8196836f7
Robert Crooks <rcrooks@brightcove.com>
2020-03-02 14:55:45 -0500

    try again

--- _site/assets/js/talla-zencoder.js
+++ _site/assets/js/talla-zencoder.js
@@ -4,15 +4,15 @@ var token =
 // -----
   Talla.config = Talla.init(
     appID,
-    Talla.OptionJWTCredentials(token), Talla.OptionBotName("Zencoder​ by Brightcove"),
+    Talla.OptionJWTCredentials(token), Talla.OptionBotName("Zencoder by Brightcove"),
     Talla.OptionAnonymous(true),
     Talla.OptionThemeColor('#000000'),
-    Talla.OptionDisplayName('​Zencoder Support​'),
-    Talla.OptionKnowledgeGroups(['​2233f970-dfad-4c06-bb6e-fdf4ddba4506​']),
+    Talla.OptionDisplayName('Zencoder Support'),
+    Talla.OptionKnowledgeGroups(['2233f970-dfad-4c06-bb6e-fdf4ddba4506']),
     Talla.OptionWideBeacon(true),
     Talla.OptionBeaconCTA("Ask a question"),
-    Talla.OptionOpenIcon('​https://support.brightcove.com/sites/default/files/favicon-64.png​'),
-    Talla.OptionBotIcon('​https://support.brightcove.com/sites/default/files/favicon-64.png​'),
+    Talla.OptionOpenIcon('https://support.brightcove.com/sites/default/files/favicon-64.png'),
+    Talla.OptionBotIcon('https://support.brightcove.com/sites/default/files/favicon-64.png'),
     Talla.OptionAutodetectSession
   );
 Talla.config.display(Talla.OptionQuickLoadBeacon, Talla.OptionDetectParent(function() {
