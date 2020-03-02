7ab5187847294776255971884ecc254be32b325c
Robert Crooks <rcrooks@brightcove.com>
2020-03-02 14:46:05 -0500

    fix

--- assets/js/talla-zencoder.js
+++ assets/js/talla-zencoder.js
@@ -2,11 +2,11 @@ var appID = "00c26be3-4421-41e6-bb00-1dee68740f67"; // token for anonymous profi
 var token =
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODI4MDQzNDF9.RYf2x65FXvdDURzM h00i76KfUrYlsDLsw6mVuEv6zmE';
 // -----
-  Talla.config = Talla.init( 
+  Talla.config = Talla.init(
     appID,
     Talla.OptionJWTCredentials(token), Talla.OptionBotName("Zencoder​ by Brightcove"),
     Talla.OptionAnonymous(true),
-    Talla.OptionThemeColor('#000000'), 
+    Talla.OptionThemeColor('#000000'),
     Talla.OptionDisplayName('​Zencoder Support​'),
     Talla.OptionKnowledgeGroups(['​2233f970-dfad-4c06-bb6e-fdf4ddba4506​']),
     Talla.OptionWideBeacon(true),
@@ -15,7 +15,7 @@ var token =
     Talla.OptionBotIcon('​https://support.brightcove.com/sites/default/files/favicon-64.png​'),
     Talla.OptionAutodetectSession
   );
-Talla.config.display(Talla.OptionQuickLoadBeacon, Talla.OptionDetectParent(function() { 
+Talla.config.display(Talla.OptionQuickLoadBeacon, Talla.OptionDetectParent(function() {
       return document.querySelector('#talla_parent');
     }
   )
