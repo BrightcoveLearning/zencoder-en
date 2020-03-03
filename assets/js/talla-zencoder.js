6408ae049da04b7f0fd0c97f77403d3298c3e455
Robert Crooks <rcrooks@brightcove.com>
2020-03-03 09:46:09 -0500

    update talla

--- assets/js/talla-zencoder.js
+++ assets/js/talla-zencoder.js
@@ -1,3 +1,7 @@
+---
+layout: null
+---
+
 var appID = "00c26be3-4421-41e6-bb00-1dee68740f67"; // token for anonymous profile
 var token =
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODI4MDQzNDF9.RYf2x65FXvdDURzM h00i76KfUrYlsDLsw6mVuEv6zmE';
@@ -6,11 +10,11 @@ var token =
     appID,
     Talla.OptionJWTCredentials(token), Talla.OptionBotName("Zencoder by Brightcove"),
     Talla.OptionAnonymous(true),
-    Talla.OptionThemeColor('#000000'),
+    Talla.OptionThemeColor('{{ site.product_color }}'),
     Talla.OptionDisplayName('Zencoder Support'),
     Talla.OptionKnowledgeGroups(['2233f970-dfad-4c06-bb6e-fdf4ddba4506']),
     Talla.OptionWideBeacon(true),
-    Talla.OptionBeaconCTA("Ask a question"),
+    Talla.OptionBeaconCTA("Search Zencoder Docs"),
     Talla.OptionOpenIcon('https://support.brightcove.com/sites/default/files/favicon-64.png'),
     Talla.OptionBotIcon('https://support.brightcove.com/sites/default/files/favicon-64.png'),
     Talla.OptionAutodetectSession
